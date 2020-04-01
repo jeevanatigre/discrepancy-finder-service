cab69ec06b825a70912ab4c1fd836d05b2eb8420
Puja Arora <62877973+Puja-Arora@users.noreply.github.com>
2020-04-01 19:05:35 +0530

    Update WriteWorkBook.java
    Updated WriteWorkBook code for Descrepency model read and auto creation of output directory

--- src/com/dfs/impl/WriteWorkBook.java
+++ src/com/dfs/impl/WriteWorkBook.java
@@ -1,6 +1,7 @@
 package com.dfs.impl;
 
 import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,11 +12,13 @@ import java.util.Scanner;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.dfs.model.DiscrepancyRules;
 import com.dfs.model.Rule;
+import com.dfs.model.Discrepancy;
 import com.dfs.util.Constants;
 import org.apache.poi.ss.usermodel.Font;
 
 import java.io.FileOutputStream;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -27,52 +30,73 @@ import org.apache.poi.ss.usermodel.Row;
 @SuppressWarnings("all")
 public class WriteWorkBook {
 	public static void main(String[] args) throws IOException {
+		Discrepancy discrepancy = new Discrepancy();
 	try {
-		readXml();
+		copyFile();
+		addInExcel(discrepancy);
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
 	}
-	public static void addInExcel(String filePattern, File fileName,String lineNum, String cateGory, String fileType, String textPattern, String getRepl) throws InvalidFormatException, IOException {
+	
+	public static void addInExcel(Discrepancy discrepancy) throws InvalidFormatException, IOException {
 		FileInputStream inputStream;
+		FileInputStream myxls = new FileInputStream("discrepancy-output-files\\discrepancy-list.xls");
+	    HSSFWorkbook workBook = new HSSFWorkbook(myxls);
+	    String  lineNo ="";
+	    String category = "";
+	    String pattern = "";
+	    String fileType = "";
+		String complexityValue = "";
+		String ruleType = "";
+		String recommendation = "";
+		String autoRemediation = "";
+		String timeSavingsInMin = "";
 		try {
-			FileInputStream myxls = new FileInputStream("discrepancy-output-files\\discrepancy-list.xls");
-		       HSSFWorkbook workBook = new HSSFWorkbook(myxls);
+			
 		       HSSFSheet worksheet = workBook.getSheetAt(0);
 		       int lastRow=worksheet.getLastRowNum();
 		       System.out.println(lastRow);
 		       Row row = worksheet.createRow(++lastRow);
-		       if(null!=filePattern && null!=fileName.toString() && null!=lineNum 
-		    		   && null!=cateGory && null!=fileType && null!=textPattern && null!=getRepl) {
-		       row.createCell(0).setCellValue(filePattern);
-		       row.createCell(1).setCellValue(fileName.toString());
-		       row.createCell(2).setCellValue(lineNum);
-		       if(cateGory.equalsIgnoreCase(Constants.MANDATORY)) {
+		       pattern = (discrepancy.getPattern()== null) ? "": discrepancy.getPattern();
+		       fileType = (discrepancy.getFileType()== null) ? "": discrepancy.getFileType();
+		       ruleType = (discrepancy.getRuleType()== null) ? "": discrepancy.getRuleType();
+		       recommendation = (discrepancy.getRecommendation()== null) ? "": discrepancy.getRecommendation();
+		       complexityValue = (discrepancy.getComplexity()== null) ? "": discrepancy.getComplexity();
+		       autoRemediation = (discrepancy.getAutoRemediation()== null) ? "": discrepancy.getAutoRemediation();
+		       timeSavingsInMin = (discrepancy.getTimeSavingsInMin()== null) ? "": discrepancy.getTimeSavingsInMin();
+		       //fileName = 
+		       if(null != discrepancy.getLineNo()) {
+		      lineNo = Integer.toString(discrepancy.getLineNo());} 
+		       row.createCell(0).setCellValue(pattern);
+		       row.createCell(1).setCellValue(discrepancy.getFileName());
+		       row.createCell(2).setCellValue(lineNo);
+		       category = (discrepancy.getCategory()== null) ? "" :discrepancy.getCategory();
+		       if(category.equalsIgnoreCase(Constants.MANDATORY)) {
 		    	   CellStyle style = workBook.createCellStyle();
 		    	   style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND); 
 		    	   style.setFillForegroundColor(HSSFColor.ROSE.index);
 		    	   Font font = workBook.createFont();
 		           font.setColor(HSSFColor.RED.index);
 		           style.setFont(font);
-		    	   row.createCell(3).setCellValue(cateGory);
+		    	   row.createCell(3).setCellValue(category);
 		    	   row.getCell(3).setCellStyle(style);
-		       }else if(cateGory.equalsIgnoreCase(Constants.OPTIONAL)) {
+		       }else if(category.equalsIgnoreCase(Constants.OPTIONAL)) {
 		    	   CellStyle style = workBook.createCellStyle();
 		    	   style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND); 
 		    	   style.setFillForegroundColor(HSSFColor.LIGHT_GREEN.index);
 		    	   Font font = workBook.createFont();
 		           font.setColor(HSSFColor.GREEN.index);
 		           style.setFont(font);
-		    	   row.createCell(3).setCellValue(cateGory);
+		    	   row.createCell(3).setCellValue(category);
 		    	   row.getCell(3).setCellStyle(style);
 		       }
 		       row.createCell(4).setCellValue(fileType);
-		       row.createCell(5).setCellValue(textPattern);
-		       row.createCell(6).setCellValue(getRepl);
-		       row.createCell(7).setCellValue(1);
-		       row.createCell(8).setCellValue("yes");
-		       row.createCell(9).setCellValue(2);
-		       }
+		       row.createCell(5).setCellValue(ruleType);
+		       row.createCell(6).setCellValue(recommendation);
+		       row.createCell(7).setCellValue(complexityValue);
+		       row.createCell(8).setCellValue(autoRemediation);
+		       row.createCell(9).setCellValue(timeSavingsInMin);
 		       myxls.close();
 		       FileOutputStream output_file =new FileOutputStream(new File("discrepancy-output-files\\\\discrepancy-list.xls"));  
 		       //write changes
@@ -86,50 +110,27 @@ public class WriteWorkBook {
 		}
 	}
         
-public static void readXml() throws InvalidFormatException, IOException {
-	File jaavaRulrXml = new File("resources\\java-rules.xml");
+public static void copyFile() throws InvalidFormatException, IOException {
+	String directory = "discrepancy-output-files";
 	try {
-		List<String> xmlCodeLineList = new ArrayList<String>();
-		String filePatternVal = "";
-		String complexityValue = "";
-		String category = "";
-		String ruleType = "";
-		String textPattern = "";
-		String replatformAdvice = "";
-		Scanner scanner;
-		scanner = new Scanner(jaavaRulrXml);
-		while (scanner.hasNextLine()) {
-			xmlCodeLineList.add(scanner.nextLine().trim());
-		}
-		XmlMapper xmlMapper = new XmlMapper();
-		StringBuilder xml = new StringBuilder();
-		xmlCodeLineList.forEach(xml::append);
-		DiscrepancyRules discrepancyRules = xmlMapper.readValue(xml.toString(), DiscrepancyRules.class);
-		List<Rule> ruleList = (List<Rule>) discrepancyRules.getRule();
-        
-        if (ruleList.size() > 0) {
-			for (int temp = 0; temp < ruleList.size(); temp++) {
-				//Updated code for null pointer error
-				complexityValue = (ruleList.get(temp).getComplexity()== null) ? "": ruleList.get(temp).getComplexity().getValue();
-				filePatternVal = (ruleList.get(temp).getFile_pattern()== null) ? "" :ruleList.get(temp).getFile_pattern().getValue();
-				filePatternVal = ruleList.get(temp).getFile_pattern().getValue();
-				filePatternVal =  filePatternVal.replaceAll("[*.]","");
-				category = (ruleList.get(temp).getCategory()== null) ? "" :ruleList.get(temp).getCategory();
-				ruleType = (ruleList.get(temp).getType()== null) ? "" :ruleList.get(temp).getType();
-				textPattern = (ruleList.get(temp).getText_pattern()== null) ? "": ruleList.get(temp).getText_pattern().getValue();
-				replatformAdvice = (ruleList.get(temp).getRemediation()== null) ? "":ruleList.get(temp).getRemediation().getRecommendation();
-				addInExcel(filePatternVal,jaavaRulrXml,complexityValue
-						,category,ruleType,textPattern,replatformAdvice);
-}
+			File dir = new File(directory);
+			if (!dir.exists()) {
+				dir.mkdirs();
+				System.out.println("Directory created");
 			}
-	}catch (FileNotFoundException e) {
+			File from = new File("resources\\sample-report.xls"); 
+			File to = new File("discrepancy-output-files\\discrepancy-list.xls");
+			if(!to.exists()) {
+			FileUtils.copyFile(from, to);
+			System.out.println("file copied");
+			}else {
+			System.out.println("file exist already");
+			}
+	}catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch(NullPointerException e) {
-			// TODO Auto-generated catch block
-						e.printStackTrace();
-		}
 
 	}
+}
 }
 	
