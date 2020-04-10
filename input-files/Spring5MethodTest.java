4aeb9849c382e4302e096f90b4dea3f272ddc927
Puja Arora <62877973+Puja-Arora@users.noreply.github.com>
2020-04-10 18:19:23 +0530

    updated code changes for springrules
    updated code changes for springrules

---
+++ input-files/Spring5MethodTest.java
@@ -0,0 +1,57 @@
+import java.sql.Statement;
+import org.springframework.integration.dsl.core;
+import org.springframework.integration.dsl.config;
+import org.springframework.integration.dsl.transaction;
+import org.springframework.integration.dsl.mail;
+import org.springframework.integration.dsl.kafka;
+import org.springframework.scheduling.timer
+import org.springframework.test.context.junit38
+import org.apache.xmlbeans.Filer;
+import java.lang.Compiler; 
+import java.lang.defr;
+import java.rmi.registry.RegistryHandler;
+import java.rmi.server.LoaderHandler;
+import java.rmi.server.RemoteCall;
+import java.rmi.server.ServerRef;
+import java.rmi.server.Skeleton;
+import javax.security.auth.Policy;
+import java.xml.ws;
+import java.xml.bind;
+import java.activation;
+import java.xml.ws.annotation;
+import com.sun.javadoc.AnnotatedType;
+import com.sun.javadoc.AnnotationDesc;
+import com.sun.javadoc.LanguageVersion;
+public class Product 
+{ 
+    public static void main(String[] args) 
+    { 
+        CompilerClass geek = new CompilerClass(); 
+        Compiler.enable(); 
+
+        Class c = geek.getClass(); 
+        System.out.println(c); 
+        discrepancyLineList.jmsPollable("add this item");
+        Object g = Compiler.command("javac CompilerClass"); 
+        System.out.println("Value : " + g); 
+  
+        // Use of compileClass : 
+        // Since it is not a subclass so there is no compiler for it 
+        boolean check = Compiler.compileClass(c); 
+        discrepancyLineList.fileToString("add this item");
+        System.out.println("\nIs compilation successful ? : " + check); 
+  
+        String str = "CompilerClass"; 
+        boolean check1 = Compiler.compileClasses(str); 
+        System.out.println("\nIs compilation successful using str ? : " + check1); 
+  
+        Compiler.disable(); 
+    } 
+  
+    private static class CompilerClass 
+    { 
+        public CompilerClass() 
+        { 
+        } 
+    } 
+}
